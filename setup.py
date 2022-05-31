33c34b1dea4411358714493d45203c1f2d2cf36c
J.C. Jones <jc@letsencrypt.org>
2022-05-31 12:03:33 -0700

    Version bump the main branch to 0.3.3 unstable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="mariadb-sequential-partition-manager",
-    version="0.3.2",
+    version="0.3.3",
     description="Manage DB partitions based on sequential IDs",
     long_description="Manage MariaDB Partitions based on sequential IDs",
     classifiers=[
