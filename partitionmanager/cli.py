a47773efeebcbf18ef23e10001382dd2feb3e54d
J.C. Jones <jc@letsencrypt.org>
2024-02-29 12:59:38 -0700

    Stray merge

--- partitionmanager/cli.py
+++ partitionmanager/cli.py
@@ -369,8 +369,6 @@ def do_partition(conf):
         except partitionmanager.types.DatabaseCommandException as e:
             log.warning("Failed to automatically handle %s: %s", table, e)
             metrics.add("alter_errors", table.name, 1)
-        except partitionmanager.types.TableEmptyException:
-            log.warning("Table %s appears to be empty. Skipping.", table)
         except (ValueError, Exception) as e:
             log.warning("Failed to handle %s: %s", table, e)
             metrics.add("alter_errors", table.name, 1)
